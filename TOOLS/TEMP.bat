ab2a4a387224e5162d6a6844c5c6c9a58f10cac4
KALI-LINUX404 <94560865+KALI-LINUX404@users.noreply.github.com>
2025-08-26 07:28:51 +0300

    Update TEMP.bat

--- TOOLS/TEMP.bat
+++ TOOLS/TEMP.bat
@@ -9,11 +9,15 @@ for /d %%p in ("C:\Windows\Temp\*.*") do rmdir "%%p" /s /q
 echo Done with C:\Windows\Temp
 echo.
 
-REM Clean C:\Users\Administrator\AppData\Local\Temp
-echo Cleaning C:\Users\Administrator\AppData\Local\Temp
-del /f /s /q "C:\Users\Administrator\AppData\Local\Temp\*.*"
-for /d %%p in ("C:\Users\Administrator\AppData\Local\Temp\*.*") do rmdir "%%p" /s /q
-echo Done with C:\Users\Administrator\AppData\Local\Temp
-echo.
+REM Get current user's temp folder path
+set "tempfolder=%TEMP%"
+
+echo Temp folder: %tempfolder%
+
+REM Delete files
+del /f /s /q "%tempfolder%\*.*"
+
+REM Delete folders
+for /d %%p in ("%tempfolder%\*.*") do rmdir "%%p" /s /q
 
 echo Cleaning complete!
